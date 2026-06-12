aaa88aafdef655fc34f4ea282d85216598ae3a4f
Filip-Kapala <filip.kapala91@gmail.com>
2026-06-12 21:27:38 +0000

    add  check for occupied fields

--- app/app/App/Http/Controllers/GameController.php
+++ app/app/App/Http/Controllers/GameController.php
@@ -175,7 +175,11 @@ class GameController extends Controller
         // Once all the checks have passed, you can finally update the game board by calling
         // $game->setSpace( $x, $y, GameMark::Circle ).
         // [ The code to check if the space is free goes here ]
-
+            if ($game->getSpace($x, $y) !== GameMark::None) {
+                return response("This space is already occupied")
+                ->setStatusCode(403)
+                ->header('Content-Type', 'text/plain');
+            }
         // If the space is not free, run the code in the line below by removing the //
         //return response("This space has already been claimed!")->setStatusCode(403)->header('Content-Type', 'text/plain');
 
